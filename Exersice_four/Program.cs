1bb87d7b59c49c9dbd4912fc6848b6af261e076d
AlexBystrov <sshbystrov@gmail.com>
2023-01-14 14:34:08 +0400

    added answer for exersice 4

---
+++ Exersice_four/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if(num > 1)
+{
+while(count <= num)
+{
+Console.Write(count + " ");
+count = count + 2;
+}
+}
+else
+{
+    Console.Write($"В диапозоне между 1 и введенным вами числом ({num}) отсутсвуют четные элменты");
+}
